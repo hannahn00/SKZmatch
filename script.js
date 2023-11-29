96650fa49fedf25b29d2ae3ebfde8ac5dbb26d5a
Hannah Nguyen <hannahng004@gmail.com>
2023-11-29 09:43:19 -0500

    png start stop commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     function createCarousel(media, carouselInner) {
+        shuffleArray(media);
+
         media.forEach((item, index) => {
             let carouselItem = document.createElement("div");
+            
             carouselItem.classList.add("carousel-item");
             if (index == 0) {
             carouselItem.classList.add("active");
@@ -12,6 +15,13 @@ document.addEventListener("DOMContentLoaded", function() {
             carouselItem.append(img);
             carouselInner.append(carouselItem);
         });
+    }
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
     }
 
     // UPPER media array
@@ -34,6 +44,21 @@ document.addEventListener("DOMContentLoaded", function() {
     let SHOEScarouselInner = document.querySelector('.SHOEScarousel-inner');
     createCarousel(SHOESmedia, SHOEScarouselInner);
     
+    document.getElementById('startCarousel').addEventListener('click', function () {
+        $('#UPcarousel').carousel('cycle');
+        $('#SHIRTScarousel').carousel('cycle');
+        $('#PANTScarousel').carousel('cycle');
+        $('#SHOEScarousel').carousel('cycle');
+    });
+
+    // Handle button click to stop the carousel
+    document.getElementById('stopCarousel').addEventListener('click', function () {
+        $('#UPcarousel').carousel('pause');
+        $('#SHIRTScarousel').carousel('pause');
+        $('#PANTScarousel').carousel('pause');
+        $('#SHOEScarousel').carousel('pause');
+    });
+
     
 
 });
